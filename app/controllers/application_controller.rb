8246cf95b89ff7d19ef42a459e8f9b863ac13f2a
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-15 23:32:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
     user = User.new(username: params[:username], password: params[:password])
 
     if user.save
-      redirect '/login'
+      redirect to '/login'
     else
       redirect '/failure'
     end
