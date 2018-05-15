867cd584be568e7fe9f3fe1bd4828fb3bff70506
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-15 23:38:22 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180515232530) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
